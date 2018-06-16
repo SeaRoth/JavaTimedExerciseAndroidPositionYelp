d483e17246f3b05380936cb0fa7f46be8c274016
SeaRoth <coryr32@gmail.com>
2018-06-16 13:23:31 -0700

    Initial commit

---
+++ app/src/main/java/com/searoth/yelpandroidcodingquestion/JT.java
@@ -0,0 +1,112 @@
+package com.searoth.yelpandroidcodingquestion;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+
+public class JT {
+    /**
+     * @param args
+     *
+     * Given a List of Lists, find the color(s) that occurs the most and return an alphabetized list
+     * of the most frequent color(s)     *
+     *
+     */
+    public static void main(String args[]) {
+        List<List<String>> mainList = new ArrayList<List<String>>();
+
+        List<String> one = new ArrayList<>();
+        one.add("green");
+        one.add("blue");
+        one.add("red");
+        one.add("purple");
+
+        List<String> two = new ArrayList<>();
+        two.add("green");
+        two.add("blue");
+        two.add("red");
+        two.add("purple");
+
+        List<String> three = new ArrayList<>();
+        three.add("green");
+        three.add("blue");
+        three.add("red");
+        three.add("purple");
+
+        mainList.add(one);
+        mainList.add(two);
+        mainList.add(three);
+
+        System.out.println(topColor(mainList));
+    }
+
+    /**
+     *
+     * @param image List of Lists of Strings
+     * @return alphabatized list of the most frequent colors found in all lists
+     */
+    private static List<String> topColor(List<List<String>> image) {
+        List<String> ret = new ArrayList<>();
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+        int max = 1;
+
+        for(List<String> inner : image){
+            for(String color : inner){
+                if(map.containsKey(color)) {//does color already exist?
+                    int count = map.get(color)+1;
+                    max = count > max ? count : max;
+                    map.put(color, count);
+                }else map.put(color,1);
+            }
+        }
+        if(map.size() > 1) {
+            ret = buildColorList(map, max);//build unsorted list
+            ret = ret.size() > 1 ? sortListString(ret) : ret; //sort if size > 1
+        }
+        return ret;
+    }
+
+    /**
+     *
+     * @param map colors and their frequency
+     * @param key frequency of color to find and add to ArrayList<String>
+     * @return unsorted list of colors
+     */
+    private static List<String> buildColorList(HashMap<String, Integer> map, int key){
+        List<String> ret = new ArrayList<>();
+        Set<String> set = map.keySet();
+        for(String s : set){
+            if(map.get(s) == key)
+                ret.add(s);
+        }
+        return ret;
+    }
+
+    /**
+     *
+     * @param arr unsorted list
+     * @return sorted list using BibbleSort
+     */
+    private static List<String> sortListString(List<String> arr){
+        String temp;
+
+        for(int i = 0; i < arr.size(); i++){
+            for(int j = 0; j < arr.size(); j++){
+                if(j+1 < arr.size()) {
+                    int isBigger = arr.get(j).compareTo(arr.get(j + 1));
+                    if (isBigger > 0) {
+                        temp = arr.get(j + 1);
+                        arr.set(j + 1, arr.get(j));
+                        arr.set(j, temp);
+                    }
+                }
+            }
+        }
+        return arr;
+    }
+
+
+
+}
+
